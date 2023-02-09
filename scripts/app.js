296d64aecf44e4c03f79e9d49331fa5443560dc0
noalb99 <norozaziz99@gmail.com>
2023-02-09 09:37:10 +0300

    this my first project

--- scripts/app.js
+++ scripts/app.js
@@ -1,56 +1,104 @@
-function init(){
-     const boxes = document.querySelectorAll('.box')
-     const resetButton = document.querySelector('#restart')
-     const winnerDiv = document.querySelector('#winner')
-     let player = 'X'
+function init() {
+   const boxes = document.querySelectorAll('.box')
+   const resetButton = document.querySelector('#restart')
+   const winnerDiv = document.querySelector('#winner')
+   let player = 'X'
 
-     let board = ['','','','','','','','','']
-     const combinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-   ]
-let user = []
-
-
-   // const fullArr = board.concat(combinations)
-//1. make a new variable an array 
-//2. push inside whatever the user clicks (push)
-//3, compare the array of user clicks with the wining combination. 
-// 1. in the checkWin function make a for(let i = 0; i <= combinations.length; i++){} loop or .forEach()
-
-//2. take each element 
-
-    function checkWin (){
    
-    }
-
-   // this box
-   boxes.forEach(box => {
+   
+   boxes.forEach((box) => {
       box.addEventListener('click', handleClick)
-     })
+  })
+  function handleClick(event) {
+      if (checkWin()) {
+          return
+      }
+      if (event.target.textContent === "") {
+          event.target.textContent = player
+          player = player === 'X' ? 'O' : 'X'
+          checkWin()
+      }
+  }
 
-     function handleClick(){
-      if(this.textContent===''){
-         this.textContent = player
-         user.push()
-      player = player ==="X" ? "O" : "X"
-      } 
-      // console.log(fullArr);
-     } 
+   function checkWin() {
+      if (boxes[0].innerHTML === 'X' && boxes[1].innerHTML === 'X' && boxes[2].innerHTML === 'X') {
+         console.log(boxes[0].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[0].innerHTML + ''
+         return true
+     } if (boxes[3].innerHTML === 'X' && boxes[4].innerHTML === 'X' && boxes[5].innerHTML === 'X') {
+         console.log(boxes[3].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[3].innerHTML + ''
+         return true
+     } else if (boxes[6].innerHTML === 'X' && boxes[7].innerHTML === 'X' && boxes[8].innerHTML === 'X') {
+         console.log(boxes[6].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[6].innerHTML + ''
+         return true
+     } else if (boxes[0].innerHTML === 'X' && boxes[3].innerHTML === 'X' && boxes[6].innerHTML === 'X') {
+         console.log(boxes[0].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[0].innerHTML + ''
+         return true
+     } else if (boxes[1].innerHTML === 'X' && boxes[4].innerHTML === 'X' && boxes[7].innerHTML === 'X') {
+         console.log(boxes[1].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[1].innerHTML + ''
+         return true
+     } else if (boxes[2].innerHTML === 'X' && boxes[5].innerHTML === 'X' && boxes[8].innerHTML === 'X') {
+         console.log(boxes[2].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[2].innerHTML + ''
+         return true
+     } else if (boxes[0].innerHTML === 'X' && boxes[4].innerHTML === 'X' && boxes[8].innerHTML === 'X') {
+         console.log(boxes[0].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[0].innerHTML + ''
+         return true
+     } else if (boxes[2].innerHTML === 'X' && boxes[4].innerHTML === 'X' && boxes[6].innerHTML === 'X') {
+         console.log(boxes[2].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[2].innerHTML + ''
+         return true
+     }
+     if (boxes[0].innerHTML === 'O' && boxes[1].innerHTML === 'O' && boxes[2].innerHTML === 'O') {
+         console.log(boxes[0].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[0].innerHTML + ''
+         return true
+     } if (boxes[3].innerHTML === 'O' && boxes[4].innerHTML === 'O' && boxes[5].innerHTML === 'O') {
+         console.log(boxes[3].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[3].innerHTML + ''
+         return true
+     } else if (boxes[6].innerHTML === 'O' && boxes[7].innerHTML === 'O' && boxes[8].innerHTML === 'O') {
+         console.log(boxes[6].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[6].innerHTML + ''
+         return true
+     } else if (boxes[0].innerHTML === 'O' && boxes[3].innerHTML === 'O' && boxes[6].innerHTML === 'O') {
+         console.log(boxes[0].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[0].innerHTML + ''
+         return true
+     } else if (boxes[1].innerHTML === 'O' && boxes[4].innerHTML === 'O' && boxes[7].innerHTML === 'O') {
+         console.log(boxes[1].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[1].innerHTML + ''
+         return true
+     } else if (boxes[2].innerHTML === 'O' && boxes[5].innerHTML === 'O' && boxes[8].innerHTML === 'O') {
+         console.log(boxes[2].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[2].innerHTML + ''
+         return true
+     } else if (boxes[0].innerHTML === 'O' && boxes[4].innerHTML === 'O' && boxes[8].innerHTML === 'O') {
+         console.log(boxes[0].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[0].innerHTML + ''
+         return true
+     } else if (boxes[2].innerHTML === 'O' && boxes[4].innerHTML === 'O' && boxes[6].innerHTML === 'O') {
+         console.log(boxes[2].innerHTML + 'has won')
+         winnerDiv.innerHTML = boxes[2].innerHTML + ''
+         return true
+     }
+   }
 
-     resetButton.addEventListener('click',resetBoard)
-      function resetBoard() {
+
+
+   function resetBoard() {
       boxes.forEach(box => {
          box.textContent = ""
+         winnerDiv.textContent = ""
       })
-     }
+   }
+   resetButton.addEventListener('click', resetBoard)
 }
 
 
-window.addEventListener('DOMContentLoaded', init)
+window.addEventListener('DOMContentLoaded', init) 
